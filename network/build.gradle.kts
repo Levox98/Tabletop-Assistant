d31752ae3f89e3c270f741a1b1b17c1f0bd8ac5f
Levox98 <ivanov.ks101@gmail.com>
2024-08-19 22:59:24 +0500

    added net libs

--- network/build.gradle.kts
+++ network/build.gradle.kts
@@ -34,9 +34,13 @@ android {
 
 dependencies {
 
+	implementation(libs.retrofit)
+	implementation(libs.okhttp)
+
 	implementation(libs.androidx.core.ktx)
 	implementation(libs.androidx.appcompat)
 	implementation(libs.material)
+
 	testImplementation(libs.junit)
 	androidTestImplementation(libs.androidx.junit)
 	androidTestImplementation(libs.androidx.espresso.core)
