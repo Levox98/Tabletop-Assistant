b5861d39d384312373b6f78f7413a2e3a67e1ed2
Kirill Ivanov <ivanov.ks101@gmail.com>
2024-11-18 19:54:33 +0500

    small fix

--- core-domain/src/main/java/com/tabletop_assistant/core_domain/Either.kt
+++ core-domain/src/main/java/com/tabletop_assistant/core_domain/Either.kt
@@ -1,10 +1,22 @@
 package com.tabletop_assistant.core_domain
 
-sealed class Either<T> {
+sealed class Either<out T> {
+
+	fun isLoading(): Boolean {
+		return this is Loading
+	}
+
+	fun isSuccess(): Boolean {
+		return this is Success
+	}
+
+	fun isError(): Boolean {
+		return this is Error
+	}
 
 	data object Loading : Either<Nothing>()
 
-	data class Success<T>(val data: T?) : Either<T>()
+	data class Success<out T>(val data: T?) : Either<T>()
 
-	data class Error<T>(val error: Throwable) : Either<T>()
+	data class Error<out T>(val error: Throwable) : Either<T>()
 }
