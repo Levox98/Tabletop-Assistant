ce564949c40a6f59c3436c91cc705e51a25e4d10
Kirill Ivanov <ivanov.ks101@gmail.com>
2024-11-30 19:27:50 +0500

    fixed formatting

--- core-common/src/main/java/com/tabletop_assistant/core_common/BaseViewModel.kt
+++ core-common/src/main/java/com/tabletop_assistant/core_common/BaseViewModel.kt
@@ -6,19 +6,21 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import timber.log.Timber
 
-open class BaseViewModel<T: BaseViewModelState>(initialState: T) : ViewModel() {
+open class BaseViewModel<T : BaseViewModelState>(initialState: T) : ViewModel() {
 
     private var cachedState: T? = null
 
     private val _state = MutableStateFlow<T>(initialState)
     val state: StateFlow<T>
         get() {
-            Timber.tag("${LOG_TAG}_getState").v("getting state of type ${_state.value.javaClass.canonicalName}: ${_state.value}")
+            Timber.tag("${LOG_TAG}_getState")
+                .v("getting state of type ${_state.value.javaClass.canonicalName}: ${_state.value}")
             return _state.asStateFlow()
         }
 
     fun updateState(newState: T) {
-        Timber.tag("${LOG_TAG}_updateState").v("${_state.value.javaClass.canonicalName} newState: $newState")
+        Timber.tag("${LOG_TAG}_updateState")
+            .v("${_state.value.javaClass.canonicalName} newState: $newState")
         _state.value = newState
     }
 
