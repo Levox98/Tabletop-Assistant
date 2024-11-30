cb221dc339079bd8eaf34a73b4f9b8ab23526836
Kirill Ivanov <ivanov.ks101@gmail.com>
2024-11-30 16:25:05 +0500

    added some viewmodel functionality

--- core-common/src/main/java/com/tabletop_assistant/core_common/BaseViewModel.kt
+++ core-common/src/main/java/com/tabletop_assistant/core_common/BaseViewModel.kt
@@ -5,11 +5,15 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 
-open class BaseViewModel<out T: BaseViewModelState>(initialState: T) : ViewModel() {
+open class BaseViewModel<T: BaseViewModelState>(initialState: T) : ViewModel() {
 
     private var cachedState: T? = null
 
     private val _state = MutableStateFlow<T>(initialState)
     val state: StateFlow<T>
         get() = _state.asStateFlow()
+
+    fun updateState(newState: T) {
+        _state.value = newState
+    }
 }
